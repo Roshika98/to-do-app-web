dffd8bfd53d18b9f6b349cad11f6dfec7a82f0e6
Roshika Perera <pereraroshika98@gmail.com>
2025-02-28 01:06:22 +0530

    enhance auth guard to redirect unauthenticated users to login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const accessToken = authService.getAccessToken();
+  if (!accessToken) {
+    router.navigate(['/login']);
+    return false;
+  }
   return true;
 };
